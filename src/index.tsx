9a07f04041005c12f3eb190990250abe672d602f
MyuB <mjycoco0@naver.com>
2023-12-27 21:52:42 +0900

    feat: 로그인을 위한 페이지와 컴포넌트 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import firebase from "firebaseApp";
-
-console.log(firebase);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
