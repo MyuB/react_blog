8581c1b527d7ac778bcf3673b446f01343f0b439
MyuB <mjycoco0@naver.com>
2024-01-14 20:01:55 +0900

    feat: context API 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthContextProvider } from "context/AuthContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <AuthContextProvider>
+    <Router>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Router>
+  </AuthContextProvider>
 );
