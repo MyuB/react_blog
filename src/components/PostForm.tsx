fa4cffe5c592ba5b443fbe4dd89da739feaf3986
MyuB <mjycoco0@naver.com>
2024-01-14 21:26:51 +0900

    feat: firestore에 query 및 색인을 적용하여 카테고리 탭 기능

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -4,7 +4,7 @@ import { collection, addDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 import AuthContext from "context/AuthContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { PostProps } from "./PostList";
+import { CATEGORIES, CategoryType, PostProps } from "./PostList";
 
 export default function PostForm() {
   const params = useParams();
@@ -13,6 +13,7 @@ export default function PostForm() {
   const [title, setTitle] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [category, setCategory] = useState<CategoryType>("Frontend");
   const navigate = useNavigate();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -29,6 +30,7 @@ export default function PostForm() {
             minute: "2-digit",
             second: "2-digit",
           }),
+          category: category,
         });
         toast?.success("게시글을 수정했습니다.");
         navigate(`/posts/${post.id}`);
@@ -44,6 +46,7 @@ export default function PostForm() {
           }),
           email: user?.email,
           uid: user?.uid,
+          category: category,
         });
 
         toast?.success("게시글을 생성했습니다.");
@@ -60,6 +63,7 @@ export default function PostForm() {
       const docRef = doc(db, "posts", id);
       const docSnap = await getDoc(docRef);
       setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+      setCategory(post?.category as CategoryType);
     }
   };
 
@@ -76,23 +80,17 @@ export default function PostForm() {
   }, [post]);
 
   const onChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     const {
       target: { name, value },
     } = e;
-
-    if (name === "title") {
-      setTitle(value);
-    }
-
-    if (name === "summary") {
-      setSummary(value);
-    }
-
-    if (name === "content") {
-      setContent(value);
-    }
+    if (name === "title") setTitle(value);
+    if (name === "summary") setSummary(value);
+    if (name === "content") setContent(value);
+    if (name === "category") setCategory(value as CategoryType);
   };
 
   return (
@@ -107,6 +105,22 @@ export default function PostForm() {
           onChange={onChange}
           value={title}
         />
+      </div>
+      <div className="form__block">
+        <label htmlFor="category">카테고리</label>
+        <select
+          name="category"
+          id="category"
+          onChange={onChange}
+          defaultValue={category}
+        >
+          <option value="">카테고리를 선택해주세요</option>
+          {CATEGORIES?.map((category) => (
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="form__block">
         <label htmlFor="summary">요약</label>
