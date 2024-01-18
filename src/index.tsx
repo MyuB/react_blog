c1ef1b36a9ef7bc40d8ccf3928d9773268f2928f
MyuB <mjycoco0@naver.com>
2024-01-18 20:44:52 +0900

    style: 다크모드의 css 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "context/AuthContext";
+import { ThemeContextProvider } from "context/ThemeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
